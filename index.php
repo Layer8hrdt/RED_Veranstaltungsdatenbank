1d93c5aa75675a54b16e29c3dfd1b14f8821b4e6
Kevin Hardt <kevinhardt7@gmail.com>
2025-12-15 11:17:31 +0100

    Nav Leiste 1.0

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regionale Event Datenbank</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <h1 id="hometitle">RED</h1>
+    <a href="#hometitle"><p id="startNav">Start</p></a>
+    <p id="TopEventsNav">Top-Events</p>
+    <p id="WohinAmWochenende">Wohin am Wochenende?</p>
+    <p id="addEvent"><button>Event hinzufügen</button></p>
+    
+</body>
+
+</html>
